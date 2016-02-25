23f7a5aad175089b783f6306d991c25a683b8b35
FateMouse <204894332@qq.com>
2016-02-25 23:38:23 +0800

    ExampleCode

---
+++ USER/main.c
@@ -0,0 +1,161 @@
+#include "led.h"
+#include "delay.h"
+#include "key.h"
+#include "sys.h"
+#include "lcd.h"
+#include "usart.h"	 
+#include "usmart.h"
+#include "sram.h"
+#include "malloc.h"
+#include "tsensor.h"
+#include "rtc.h"
+#include "enc28j60.h" 	 
+#include "lwip/netif.h"
+#include "lwip_comm.h"
+#include "lwipopts.h"
+#include "timer.h"
+#include "includes.h"
+#include "httpd.h"
+
+/************************************************
+ ALIENTEK MiniSTM32开发板网络实验
+ WebServer实验
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
+
+//LED任务
+//任务优先级
+#define LED_TASK_PRIO		9
+//任务堆栈大小
+#define LED_STK_SIZE		64
+//任务堆栈
+OS_STK	LED_TASK_STK[LED_STK_SIZE];
+//任务函数
+void led_task(void *pdata);  
+
+//在LCD上显示地址信息任务
+//任务优先级
+#define DISPLAY_TASK_PRIO	10
+//任务堆栈大小
+#define DISPLAY_STK_SIZE	128
+//任务堆栈
+OS_STK	DISPLAY_TASK_STK[DISPLAY_STK_SIZE];
+//任务函数
+void display_task(void *pdata);
+
+//START任务
+//任务优先级
+#define START_TASK_PRIO		11
+//任务堆栈大小
+#define START_STK_SIZE		128
+//任务堆栈
+OS_STK START_TASK_STK[START_STK_SIZE];
+//任务函数
+void start_task(void *pdata); 
+//在LCD上显示地址信息
+//mode:1 显示DHCP获取到的地址
+//	  其他 显示静态地址
+void show_address(u8 mode)
+{
+	u8 buf[30];
+	if(mode==2)
+	{
+		sprintf((char*)buf,"DHCP IP :%d.%d.%d.%d",lwipdev.ip[0],lwipdev.ip[1],lwipdev.ip[2],lwipdev.ip[3]);						//打印动态IP地址
+		LCD_ShowString(30,130,210,16,16,buf); 
+		sprintf((char*)buf,"DHCP GW :%d.%d.%d.%d",lwipdev.gateway[0],lwipdev.gateway[1],lwipdev.gateway[2],lwipdev.gateway[3]);	//打印网关地址
+		LCD_ShowString(30,150,210,16,16,buf); 
+		sprintf((char*)buf,"NET MASK:%d.%d.%d.%d",lwipdev.netmask[0],lwipdev.netmask[1],lwipdev.netmask[2],lwipdev.netmask[3]);	//打印子网掩码地址
+		LCD_ShowString(30,170,210,16,16,buf); 
+	}
+	else 
+	{
+		sprintf((char*)buf,"Static IP:%d.%d.%d.%d",lwipdev.ip[0],lwipdev.ip[1],lwipdev.ip[2],lwipdev.ip[3]);						//打印动态IP地址
+		LCD_ShowString(30,130,210,16,16,buf); 
+		sprintf((char*)buf,"Static GW:%d.%d.%d.%d",lwipdev.gateway[0],lwipdev.gateway[1],lwipdev.gateway[2],lwipdev.gateway[3]);	//打印网关地址
+		LCD_ShowString(30,150,210,16,16,buf); 
+		sprintf((char*)buf,"NET MASK:%d.%d.%d.%d",lwipdev.netmask[0],lwipdev.netmask[1],lwipdev.netmask[2],lwipdev.netmask[3]);	//打印子网掩码地址
+		LCD_ShowString(30,170,210,16,16,buf); 
+	}	
+}
+
+ int main(void)
+{	 
+	delay_init();	    	 	//延时函数初始化	  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
+	uart_init(115200);	 		//串口初始化为115200
+	LED_Init();		  			//初始化与LED连接的硬件接口
+	KEY_Init();					//初始化按键
+	LCD_Init();			   		//初始化LCD 
+	RTC_Init();					//RTC初始化
+	T_Adc_Init();				//ADC初始化
+	usmart_dev.init(72);		//初始化USMART	
+	FSMC_SRAM_Init();			//初始化外部SRAM	 
+	my_mem_init(SRAMIN);		//初始化内部内存池
+	my_mem_init(SRAMEX);		//初始化外部内存池
+ 	POINT_COLOR=RED;			//设置字体为红色 
+	LCD_ShowString(30,30,200,16,16,"ENC28J60+STM32");	
+	LCD_ShowString(30,50,200,16,16,"WebServer Test");	
+	LCD_ShowString(30,70,200,16,16,"ATOM@ALIENTEK");
+	LCD_ShowString(30,90,200,16,16,"2015/4/27");  
+	POINT_COLOR = BLUE; 	//蓝色字体
+	
+	OSInit();				//UCOS初始化
+	while(lwip_comm_init()) //lwip初始化
+	{
+		LCD_ShowString(30,110,200,20,16,"LWIP Init Falied!");
+		delay_ms(1200);
+		LCD_Fill(30,110,230,130,WHITE); //清除显示
+		LCD_ShowString(30,110,200,16,16,"Retrying...");  
+	}
+	LCD_ShowString(30,110,200,20,16,"LWIP Init Success!");	//lwip初始化成功
+	httpd_init(); 
+	OSTaskCreate(start_task,(void*)0,(OS_STK*)&START_TASK_STK[START_STK_SIZE-1],START_TASK_PRIO);
+	OSStart(); //开启UCOS
+}
+
+//start任务
+void start_task(void *pdata)
+{
+	OS_CPU_SR cpu_sr;
+	pdata = pdata ;
+	
+	OSStatInit();  			//初始化统计任务
+	OS_ENTER_CRITICAL();  	//关中断
+	OSTaskCreate(led_task,(void*)0,(OS_STK*)&LED_TASK_STK[LED_STK_SIZE-1],LED_TASK_PRIO); 	//创建LED任务
+	OSTaskCreate(display_task,(void*)0,(OS_STK*)&DISPLAY_TASK_STK[DISPLAY_STK_SIZE-1],DISPLAY_TASK_PRIO); //显示任务
+	OSTaskSuspend(OS_PRIO_SELF); //挂起start_task任务
+	OS_EXIT_CRITICAL();  //开中断
+}
+
+//显示地址等信息
+void display_task(void *pdata)
+{
+	while(1)
+	{ 
+#if LWIP_DHCP									//当开启DHCP的时候
+		if(lwipdev.dhcpstatus != 0) 			//开启DHCP
+		{
+			show_address(lwipdev.dhcpstatus );	//显示地址信息
+			OSTaskSuspend(OS_PRIO_SELF); 		//显示完地址信息后挂起自身任务
+		}
+#else
+		show_address(0); 						//显示静态地址
+		OSTaskSuspend(OS_PRIO_SELF); 			//显示完地址信息后挂起自身任务
+#endif //LWIP_DHCP
+		OSTimeDlyHMSM(0,0,0,100);
+	}
+}
+
+//led任务
+void led_task(void *pdata)
+{
+	while(1)
+	{
+		LED0 = !LED0;
+		OSTimeDlyHMSM(0,0,0,500);  //延时500ms
+ 	}
+}
---
+++ readme.txt
@@ -0,0 +1,12 @@
+
+本实验将实现如下功能：本实验采用LWIP的NETCONN编程方法实现WEB Server功能，默认开启DHCP自动获取IP地址。在浏览器中数据开发板的IP地址就可以访问WEB Server网页了。可以在网页可以控制开发板的LED、蜂鸣器、查看AD值和RTC时钟。
+
+注意：本例程使用了UCOS操作系统，默认开启DHCP,本例程为MiniSTM32开发板ENC28J60网络实验例程。
+
+            	广州市星翼电子科技有限公司
+                电话：020-38271790
+                传真：020-36773971
+	       	购买：http://shop62103354.taobao.com
+                      http://shop62057469.taobao.com
+                公司网站：www.alientek.com
+         	技术论坛：www.openedv.com
